18f3ab6fccaedaf966454807099378d8f4159394
Cody Z <codyzupnick@gmail.com>
2017-11-07 10:54:43 -0500

    Added timing of the code to the results in script. Moved all functions into funcs.py, scripy.py is just a playground now. F.evaluate now returns useful statistics about the model instead of printing

--- Part1/funcs.py
+++ Part1/funcs.py
@@ -1,17 +1,11 @@
-import pandas as pd 
-import numpy as np
 import surprise as sp
-
-#Define the format of each of the data files
-#Movies; MovieID::Title::Genres
-moviescol = ['MovieId', 'Title', 'Genres','Action', 'Adventure',
- 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
- 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
-
-
-
-_movies = pd.read_csv('./movies.dat', sep ='::', names = moviescol, engine='python')
-_ratings = pd.read_csv('./ratings100k.dat', sep = '::', names = ['UserId', 'MovieId', 'Rating', 'Timestamp'], engine = 'python')
+from surprise import AlgoBase
+import pandas as pd
+import numpy as np
+from sklearn import cross_validation as cv
+import funcs as F
+import itertools as it
+import time
 
 def build_movie_genre_matrix(movies):
     """
@@ -39,7 +33,6 @@ def build_movie_genre_matrix(movies):
     return MovieGenres
 
         
-
 def build_user_item_matrix(ratings):
     """
     Return a USERxITEM matrix with values as the user's value for the movie, null otherwise
@@ -51,7 +44,6 @@ def build_user_item_matrix(ratings):
     return matrix
 
 
-
 def sample(ratings, n, m):
     """
     Return a smaller matrix with top n users and top m items only
@@ -60,7 +52,6 @@ def sample(ratings, n, m):
     @param m number of movies with most ratings
     @returns NxM matrix of USERxITEM ratings
     """
-
     n_users = ratings['UserId'].nunique()
     n_items = ratings['MovieId'].nunique()
 
@@ -73,8 +64,157 @@ def sample(ratings, n, m):
     return subset
 
 
+# define our own baseline algorithm that Surprise can use. Just predict the average!
+class AvgBase(AlgoBase):
+    def __init__(self):
+        AlgoBase.__init__(self)
+
+    # see Surprise#building_custom_algo
+    def train(self, trainset):
+        AlgoBase.train(self, trainset)
+        # remember DF is uid, iid, rating
+        self.mean = np.mean([rating for (_, _, rating) in self.trainset.all_ratings()])
+
+    def estimate(self, u, i):
+        return self.mean
 
 
+def normalize_user_means(ratings):
+    """
+    @param Ratings pandas dataframe
+    @returns user mean normalized dataframe
+    """
+    u_i_df = F.build_user_item_matrix(ratings)
+    means = u_i_df.mean(axis=1)
+
+    def f(row):
+        u_id = row["UserId"]
+        new_r = row["Rating"] - means[u_id]
+        return new_r
+
+    ratings['Rating'] = ratings.apply(f, axis = 1)
+    return ratings
+    
+
+# we need something to test our model against. Use above defined model.
+def train_baseline(ratings):
+    """
+    Baseline model. Same as below, return a model and data to test it on
+    @param ratings Pandas Dataframe with UserId, MovieId, Ratings
+    @returns Tuple (algorithm, testdata) basemodel that just returns the baseline estimate for user/item (adjusted for user bias)
+    """
+    train_data, test_data = cv.train_test_split(ratings, test_size = 0.20)
+
+    algo = AvgBase()
+    reader = sp.Reader(rating_scale=(1, 5))
+
+    trainset = sp.Dataset.load_from_df(ratings, reader)
+    testset = sp.Dataset.load_from_df(test_data, reader)
+
+    trainset = trainset.build_full_trainset()
+
+    algo.train(trainset)
+
+    testset = testset.build_full_trainset().build_testset()
+    return (algo, testset)
+
+
+
+# train the KNN model on subsets of the data (for cross-validation)
+def train(ratings, k_neighbors, k_folds):
+    """
+    Train a model and return it. Then we can use the model and evaluate it elsewhere
+    @param ratings dataframe pandas dataframe to train on, with columns UserId, MovieId, Ratings
+    @param k_neighbors number of neighbors to examine
+    @param k_folds number of folds for cross validation
+    @returns List of (algo, test data)
+    We can call methods such as `test` and `evaluate` on this object 
+    """
+
+    train_data, test_data = cv.train_test_split(ratings, test_size = 0.20)
+    reader = sp.Reader(rating_scale=(1, 5))
+
+    trainset = sp.Dataset.load_from_df(train_data, reader)
+    testset = sp.Dataset.load_from_df(test_data, reader)
+
+    trainset.split(n_folds = k_folds)
+
+    similarity_options = { 'name': 'pearson', 'user_based': False }
+    algo = sp.KNNWithMeans(sim_options = similarity_options, k = k_neighbors)
+
+    for trainset, _ in trainset.folds():
+        algo.train(trainset)
+
+
+    testset = testset.build_full_trainset().build_testset()
+    return (algo, testset)
+
+
+def group_predictions_by_user(predictions):
+    """
+    @param List of Surprise predictions objects
+    @returns Dict {uid: [P1, P2, ...PN]} hash mapping user id to top n predictions
+    """
+    p = sorted(predictions, key = lambda x: x.uid)
+
+    groups = {}
+    for k, g in it.groupby(p, lambda x: x.uid):
+        groups[k] = sorted(list(g), key = lambda x: x.est, reverse = True)
+
+    return groups
+
+
+# For every item that a user would be rated, in top k
+def calculate_catalog_coverage(ratings, predictions, k):
+    """
+    Calculate the catalog coverage of a model over a dataset
+    @param ratings pandas dataframe with UserId, MovieId, Ratings. Must be the same set the model was trained on
+    @param List Surprise predictions
+    @oaram k Int the top k recommendations size
+    @returns Float percentage of items recommended to at least one user
+    """
+    n_movies = ratings['MovieId'].nunique()
+
+    movies_reccommended = set() # keep track of which movies are recommended. Note we only care about the number
+
+    recommendations = group_predictions_by_user(predictions)
+    for u_id, recs in recommendations.iteritems():
+        movies_reccommended.update(map(lambda x: x.iid, recs[0:3]))
+
+    return len(movies_reccommended) / float(n_movies)
+
+
+# print some evaluative summaries
+def evaluate(algo, ratings, testset, top_k = 5):
+    """
+    @param algo Surprise algorithm the model that was trained
+    @oaram ratings The ratings it was trained on, in pandas Dataframe form (so we can calculate coverage)
+    @param testset Surprise testset object, the data held out during cross-validation
+    @returns Nested Dictionary {test: {rmse, mae}, train: {rmse, mae, cc}}
+    We can use these to build up arrays for plotting.
+    """
+
+    ret = {}
+    ret['test'] = {}
+    ret['train'] = {}
+
+    test_predictions = algo.test(testset)
+    # see how it would do on the trainset to compare, comes with the algo object
+    trainset = algo.trainset.build_testset()
+    train_predictions = algo.test(trainset)
+
+    # sticking evaluate in everything for grep, training is verbose
+    ret['test']['rmse'] = sp.accuracy.rmse(test_predictions)
+    ret['train']['rmse'] = sp.accuracy.rmse(train_predictions)
+
+    ret['test']['mae'] = sp.accuracy.mae(test_predictions)
+    ret['train']['mae'] = sp.accuracy.mae(train_predictions)
+
+    # Hackish, baseline does not have a sense of "neighbors"
+    if (algo.__module__ == "surprise.prediction_algorithms.knns"):
+        ret['test']['cc'] = calculate_catalog_coverage(ratings, test_predictions, top_k)
+
+    return ret
 
 
 
--- Part1/script.py
+++ Part1/script.py
@@ -1,12 +1,14 @@
-# Test some surprise stuff
+# # Test some surprise stuff
 
-import surprise as sp
-from surprise import AlgoBase
+# import surprise as sp
+# from surprise import AlgoBase
+# import numpy as np
+# from sklearn import cross_validation as cv
+# import itertools as it
+import time
 import pandas as pd
-import numpy as np
-from sklearn import cross_validation as cv
+import pprint as pp
 import funcs as F
-import itertools as it
 
 moviescol = ['MovieId', 'Title', 'Genres','Action', 'Adventure',
  'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
@@ -17,171 +19,10 @@ print "Evaluate: Starting the script"
 _ratings = pd.read_csv('./ratings100k.dat', sep = '::', names = ['UserId', 'MovieId', 'Rating', 'Timestamp'], engine = 'python')
 _movies = pd.read_csv('./movies.dat', sep ='::', names = moviescol, engine='python')
 
-small = F.sample(_ratings, 100, 10) # subset of 100 most popular movies users, 10 most popular movies
-# medium = F.sample(_ratings, 1000, 50)
-# large = F.sample(_ratings, 5000, 100)
-
-
-# define our own baseline algorithm that Surprise can use. Just predict the average!
-class AvgBase(AlgoBase):
-    def __init__(self):
-        AlgoBase.__init__(self)
-
-    # see Surprise#building_custom_algo
-    def train(self, trainset):
-        AlgoBase.train(self, trainset)
-        # remember DF is uid, iid, rating
-        self.mean = np.mean([rating for (_, _, rating) in self.trainset.all_ratings()])
-        print "Evaluate: Mean in base is {}".format(self.mean)
-
-    def estimate(self, u, i):
-        return self.mean
-
-
-
-def normalize_user_means(ratings):
-    """
-    @param Ratings pandas dataframe
-    @returns user mean normalized dataframe
-    """
-    # df.loc[df['line_race'] == 0, 'rating'] = 0
-    u_i_df = F.build_user_item_matrix(ratings)
-    means = u_i_df.mean(axis=1)
-
-    # df.loc[df['item'] == 'apple', 'freshness'] = apple
-
-    def f(row):
-        u_id = row["UserId"]
-        new_r = row["Rating"] - means[u_id]
-        return new_r
-
-    ratings['Rating'] = ratings.apply(f, axis = 1)
-    return ratings
-    
-    
-
-
-
-# we need something to test our model against. Use above defined model.
-def train_baseline(ratings):
-    """
-    Baseline model. Same as below, return a model and data to test it on
-    @param ratings Pandas Dataframe with UserId, MovieId, Ratings
-    @returns Tuple (algorithm, testdata) basemodel that just returns the baseline estimate for user/item (adjusted for user bias)
-    """
-    train_data, test_data = cv.train_test_split(ratings, test_size = 0.20)
-
-    algo = AvgBase()
-    reader = sp.Reader(rating_scale=(1, 5))
-
-    trainset = sp.Dataset.load_from_df(ratings, reader)
-    testset = sp.Dataset.load_from_df(test_data, reader)
-
-    trainset = trainset.build_full_trainset()
-
-    algo.train(trainset)
-
-    testset = testset.build_full_trainset().build_testset()
-    return (algo, testset)
-
-
-
-# train the KNN model on subsets of the data (for cross-validation)
-def train(ratings, k_neighbors, k_folds):
-    """
-    Train a model and return it. Then we can use the model and evaluate it elsewhere
-    @param ratings dataframe pandas dataframe to train on, with columns UserId, MovieId, Ratings
-    @param k_neighbors number of neighbors to examine
-    @param k_folds number of folds for cross validation
-    @returns List of (algo, test data)
-    We can call methods such as `test` and `evaluate` on this object 
-    """
-
-    train_data, test_data = cv.train_test_split(ratings, test_size = 0.20)
-    reader = sp.Reader(rating_scale=(1, 5))
-
-    trainset = sp.Dataset.load_from_df(train_data, reader)
-    testset = sp.Dataset.load_from_df(test_data, reader)
-
-    trainset.split(n_folds = k_folds)
-
-    similarity_options = { 'name': 'pearson', 'user_based': False }
-    algo = sp.KNNWithMeans(sim_options = similarity_options, k = k_neighbors)
-
-    for trainset, _ in trainset.folds():
-        algo.train(trainset)
-
-
-    testset = testset.build_full_trainset().build_testset()
-    return (algo, testset)
-
-
-def group_predictions_by_user(predictions):
-    """
-    @param List of Surprise predictions objects
-    @returns Dict {uid: [P1, P2, ...PN]} hash mapping user id to top n predictions
-    """
-    p = sorted(predictions, key = lambda x: x.uid)
-
-    groups = {}
-    for k, g in it.groupby(p, lambda x: x.uid):
-        groups[k] = sorted(list(g), key = lambda x: x.est, reverse = True)
-
-    return groups
-
-
-
-# FIX THIS
-# For every item that a user would be rated, in top k
-def calculate_catalog_coverage(ratings, predictions, k):
-    """
-    Calculate the catalog coverage of a model over a dataset
-    @param ratings pandas dataframe with UserId, MovieId, Ratings. Must be the same set the model was trained on
-    @param List Surprise predictions
-    @oaram k Int the top k recommendations size
-    @returns Float percentage of items recommended to at least one user
-    """
-    n_movies = ratings['MovieId'].nunique()
-
-    movies_reccommended = set() # keep track of which movies are recommended. Note we only care about the number
-
-    recommendations = group_predictions_by_user(predictions)
-    for u_id, recs in recommendations.iteritems():
-        movies_reccommended.update(map(lambda x: x.iid, recs[0:3]))
-
-
-    return len(movies_reccommended) / float(n_movies)
-
-
-# print some evaluative summaries
-def evaluate(algo, ratings, testset):
-    """
-    Print some u
-    @param algo Surprise algorithm the model that was trained
-    @oaram ratings The ratings it was trained on, in pandas Dataframe form (so we can calculate coverage)
-    @param testset Surprise testset object, the data held out during cross-validation
-    """
-    test_predictions = algo.test(testset)
-    # see how it would do on the trainset to compare, comes with the algo object
-    trainset = algo.trainset.build_testset()
-    train_predictions = algo.test(trainset)
-
-    # sticking evaluate in everything for grep, training is verbose
-    print "Evaluate: RMSE of the testset is {}".format(sp.accuracy.rmse(test_predictions))
-    print "Evaluate: RMSE of the trainset is {}".format(sp.accuracy.rmse(train_predictions))
-
-    print "Evaluate: MAE of the testset is {}".format(sp.accuracy.mae(test_predictions))
-    print "Evaluate: MAE of the trainset is {}".format(sp.accuracy.mae(train_predictions))
-
-    # Hackish, baseline does not have a sense of "neighbors"
-    if (algo.__module__ == "surprise.prediction_algorithms.knns"):
-        print "Evaluate: CC of the model is {}".format(calculate_catalog_coverage(ratings, test_predictions, 5))
-
-
-
-# run models with some different parameters and sizes
-samples = [ [1000, 10], [5000, 50], [100000, 2000], [5000000, 4000] ]
+samples = [ [1000, 100], [5000, 500]] # , [100000, 4000] ]
 k_s = [3, 5, 10, 15, 30, 40]
+top_k = 5
+all_results = []
 
 for sample in samples:
     i, j = sample
@@ -189,13 +30,31 @@ for sample in samples:
     print "Evaluating Baseline and KNN on the dataset with {} users and {} items".format(i, j)
     print "Evaluating baseline"
 
-    base, base_test = train_baseline(_dataset)
-    evaluate(base, _dataset, base_test)
+    base, base_test = F.train_baseline(_dataset)
+    base_eval = F.evaluate(base, _dataset, base_test)
+    all_results.append(base_eval)
 
     for k in k_s:
-        knn, knn_test = train(_dataset, k, 5)
+        # rough timer, not great at measuring v.fast computations but probably ok for us
+        t_0 = time.time()
+
+        knn, knn_test = F.train(_dataset, k, 5)
 
         print "Evaluating KNN with k of {}".format(k)
-        evaluate(knn, _dataset, knn_test)
+        results = F.evaluate(knn, _dataset, knn_test, top_k)
+        # add k, and sample size to results
+        results['sample'] = sample
+        results['k'] = k
+
+        # add a rough time measurement
+        t_1 = time.time()
+        elapsed = t_1 - t_0
+        results['time'] = elapsed 
+
+        all_results.append(results)
+
+_print = pp.PrettyPrinter(depth = 2, indent = 2)
+for results in all_results:
+    _print.pprint(results)
 
 
